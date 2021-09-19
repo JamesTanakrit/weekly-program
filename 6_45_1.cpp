76a6b5b03abe31dc4f7981bc565092266359e1c9
JamesTanakrit <88365807+JamesTanakrit@users.noreply.github.com>
2021-09-19 17:51:20 +0700

    Update 6_45_1.cpp

--- 6_45_1.cpp
+++ 6_45_1.cpp
@@ -1,19 +1,18 @@
-////¨§à¢ÕÂ¹¿Ñ§¡ìªÑè¹ factorial (Level 3)
-//#include<stdio.h>
-//int main() {
-//	int n, fact = 1;
-//	scanf_s("%d", &n);
-//	if (n == 0)
-//	{
-//		printf("Factorial of 0 = 1");
-//	}
-//	else
-//	{
-//		for (int i = 1; i <= n; i++)
-//		{
-//			fact = fact * i;
-//		}
-//		printf("Factorial of %d = %d", n, fact);
-//	}
-//	return 0;
-//}
+#include<stdio.h>
+int main() {
+	int n, fact = 1;
+	scanf_s("%d", &n);
+	if (n == 0)
+	{
+		printf("Factorial of 0 = 1");
+	}
+	else
+	{
+		for (int i = 1; i <= n; i++)
+		{
+			fact = fact * i;
+		}
+		printf("Factorial of %d = %d", n, fact);
+	}
+	return 0;
+}
